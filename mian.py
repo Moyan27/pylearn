ded1f816cc5435eac953549621a47e49b025c83d
zhanyan <1614116381@qq.com>
2022-12-01 16:36:39 +0000

    gg

--- mian.py
+++ mian.py
@@ -1,3 +1,7 @@
-from XianDai import Vector
+from XianDai.Vector import Vector
 
-
+if __name__=="__main__":
+    vec1=Vector([1,2,3])
+    vec2=Vector([4,5,6])
+    print(vec1.normalize())
+    print(vec1.normalize().norm())
