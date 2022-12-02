89c31502b4ed8daefa646af5f8162de0afd16045
zhanyan <1614116381@qq.com>
2022-12-02 11:37:53 +0800

    gg

--- mian.py
+++ mian.py
@@ -6,6 +6,4 @@ if __name__=="__main__":
     matrix1=Matrix([[1,2,7],[3,4,9]])
     matrix2=Matrix([[2,3,4],[4,5,7],[3,6,2]])
     vec1=Vector([3,4,6])
-    print(matrix1.dot(matrix2))
-    print(matrix1.dot(vec1))
-    print(matrix2.dot(vec1))
+    vec2=Vector([4,5,6])
